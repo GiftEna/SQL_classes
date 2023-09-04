fb94864966fb49c8975def4576b1b16324a65cc3
Gift Enabulele Asemota <giftenabulele@gmail.com>
2023-09-04 19:20:15 +0200

    first commit

---
+++ first_task.sql
@@ -0,0 +1,5 @@
+SELECT * FROM customers WHERE gender = 'Female'; 
+SELECT customer_name, zip_code FROM customers;
+SELECT price from products WHERE colour = 'red';
+-- to make sure the selcted colour is really red  SELECT price, colour from products WHERE colour = 'red';
+SELECT product_id, product_name, description from products WHERE colour = 'red';
